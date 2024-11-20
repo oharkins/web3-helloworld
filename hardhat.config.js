af561eceba8167a0c4894e364d3972c138fa54b8
Odis Harkins <odisjamesharkins@gmail.com>
2024-11-19 17:29:35 -0700

    Inital Commit

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.27",
+   defaultNetwork: "sepolia",
+   networks: {
+      hardhat: {},
+      sepolia: {
+        url: process.env.API_URL+process.env.API_KEY,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
+   },
+}
